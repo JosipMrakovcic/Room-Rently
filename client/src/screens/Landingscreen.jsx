4d17946abce697ba0f396b36f0fd9bf02abd49db
Jakov Zekic <jakov.zekic@fer.unizg.hr>
2025-10-10 18:52:19 +0200

    Dodan Landing screen za glavnu stranicu
    Vizualna animacija i glavni tekst

---
+++ client/src/screens/Landingscreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Landingscreen.css';
+function LandingScreen() {
+  return (
+    <div className='row landing'>
+        <div className='col-md-12'>
+            <h2>Hotel-Rently</h2>
+            <h1>"placeholder text"</h1>
+            
+            <button>Get Started</button>
+            
+
+        </div>
+    
+    
+    
+    </div>
+  )
+}
+
+export default LandingScreen

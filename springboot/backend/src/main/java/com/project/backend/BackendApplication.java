b2ae89e674281db992393a88f643f862e60c3b94
Noa-Resetar <noa.resetar@fer.unizg.hr>
2025-10-27 16:00:53 +0100

    Radi backend i baza
    Maknuo sam sve nepotrebne stvari kod testiranja backenda bez baze.

--- springboot/backend/src/main/java/com/project/backend/BackendApplication.java
+++ springboot/backend/src/main/java/com/project/backend/BackendApplication.java
@@ -1,7 +1,5 @@
 package com.project.backend;
 
-import com.project.backend.model.ApiResponse;
-import com.project.backend.model.User;
 import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class BackendApplication {
 
-	public User newUser;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -23,24 +20,4 @@ public class BackendApplication {
 		return String.format("Hello %s", name);
 	}
 
-	@PostMapping("/user")
-	public String userInformation(@RequestBody String infoUser) {
-
-		JSONObject obj = new JSONObject(infoUser);
-
-		String id = obj.getString("sub");
-		String name = obj.getString("name");
-		String email = obj.getString("email");
-
-		newUser = new User(id, name, email);
-		return infoUser;
-	}
-
-	@GetMapping("/user")
-	public ApiResponse userInformation(){
-		ApiResponse res=new ApiResponse();
-		res.setStatus(true);
-		res.setMessage(newUser.getName() + " " + newUser.getId() + " " + newUser.getEmail());
-		return res;
-	}
 }

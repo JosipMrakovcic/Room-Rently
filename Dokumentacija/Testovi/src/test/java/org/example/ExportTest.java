6eacb3c792b756adfcac2fd563c72798a0cedb84
karlo-zivkovic1 <karlo.zivkovic@fer.unizg.hr>
2026-01-19 16:59:20 +0100

    Dodan kod za test: 3. Rubni uvjet - Izvoz statistike za period bez rezervacija(Owner)

---
+++ Dokumentacija/Testovi/src/test/java/org/example/ExportTest.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.time.Duration;
+import java.util.Collections;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ExportTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+
+        // novi profil dostupan samo Seleniumu
+        // zamijeni usera stvarnim userom računala
+        options.addArguments("--user-data-dir=C:\\Users\\User\\Desktop\\SeleniumProfile");
+
+        // ovo maskira Selenium da ga Google ne prepozna kao bota
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+
+        driver = new ChromeDriver(options);
+        // Selenium će automatski čekati do 10 sekundi za svaki element u kodu
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void teardown() {
+        //driver.quit();
+    }
+
+    @Test
+    @DisplayName("Rubni uvjet: Izvoz statistike za period bez rezervacija")
+    void testSuccessfulReservation() {
+        driver.get("https://room-rently-v2.netlify.app");
+        driver.findElement(By.tagName("button")).click();
+        driver.findElement(By.cssSelector(".navButton:nth-child(3)")).click(); // Dashboard
+        driver.findElement(By.cssSelector(".month-dropdown")).click();
+        driver.findElement(By.cssSelector("option:nth-child(5)")).click(); // April
+        driver.findElement(By.cssSelector(".export-pdf-btn")).click();
+        driver.findElement(By.cssSelector(".export-xlsx-btn")).click();
+        driver.findElement(By.cssSelector(".export-xml-btn")).click();
+        driver.findElement(By.cssSelector(".tab-btn:nth-child(2)")).click(); // Reservations
+        driver.findElement(By.cssSelector(".export-pdf-btn")).click();
+        driver.findElement(By.cssSelector(".export-xlsx-btn")).click();
+        driver.findElement(By.cssSelector(".export-xml-btn")).click();
+        driver.findElement(By.cssSelector(".return-btn")).click();
+
+        // provjera URL-a
+        String trenutniUrl = driver.getCurrentUrl();
+        Assertions.assertEquals("https://room-rently-v2.netlify.app/main", trenutniUrl, "URL se ne podudara!");
+    }
+}
